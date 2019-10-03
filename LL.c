6f7e5affe05053eb034a814121e66b6978782b0c
rmmcd <ryan_mcdermott@student.uml.edu>
2019-10-03 18:56:56 -0400

    "completed task 20, was not sure if it was correct but i know now."

--- LL.c
+++ LL.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
+#include <stdbool.h>
 
-enum tag {
+typedef enum {
     JNULL,
     JIF,
     JNUM,
@@ -11,10 +12,10 @@ enum tag {
     KIF,
     KAPP,
     JCONS,
-};
+} Tag ;
 
 typedef struct {
-    enum tag;
+    enum Tag tag;
 } expr;
 
 typedef struct {
@@ -23,30 +24,77 @@ typedef struct {
 
 } JNum;
 
+expr* make_JNum(int number) {
+
+	JNum* p = malloc(sizeof(JNum));
+	p->head.tag = JNUM;
+	p->n = number;
+	return p;
+
+}
+
 typedef struct{
     expr head;
     bool b;
 
 } JBool;
 
+expr* make_JBool(bool boolean) {
+
+	JBool* p = malloc(sizeof(JBool));
+	p->head.tag = JBOOL;
+	p->b = boolean;
+	return p;
+
+}
+
 typedef struct{
     expr head;
-    char p
+	char p;
 
 } JPrim;
+
+expr* make_JPrim(char prim) {
+
+	JPrim* p = malloc(sizeof(JPrim));
+	p->head.tag = JPRIM;
+	p->p = prim;
+	return p;
+
+}
 
 typedef struct{
 
     expr head;
     expr *c, *t, *f;
 
-} JIF;
+} JIf;
+
+expr* make_JIf(expr* c, expr* t, expr * f) {
+
+	JIf *p = malloc(sizeof(JIf));
+	p->head.tag = JIF;
+	p->c = c;
+	p->t = t;
+	p->f = f;
+	return p;
+
+}
 
 typedef struct{
 
     expr head;
-	expr* func, * args;
+	expr* func;
+	expr* args;
 
 } JApp;
 
+expr* make_JApp(expr* func, expr* args) {
 
+	JApp *p = malloc(sizeof(JApp));
+	p->head.tag = JAPP;
+	p->func = func;
+	p->args = args;
+	return p;
+
+}
